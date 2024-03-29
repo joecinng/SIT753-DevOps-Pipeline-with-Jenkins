788d61db1c971926cfde390f24992f48340b1b55
Joe Cin Ng <joe5763481@gmail.com>
2024-03-29 22:46:39 +1100

    Task done

--- public/js/script.js
+++ public/js/script.js
@@ -1,51 +1,54 @@
-const cardList = [
-    {
-        title: "Kitten 1",
-        image: "images/cat-1.png", 
-        link: "About Kitten 1",
-        desciption: "Demo desciption about kitten 1"
-    },
-    {
-        title: "Kitten 2",
-        image: "images/cat-2.png", 
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "images/cat-3.png", 
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-];
-
 const addCards = (items) => { 
     items.forEach(item => {
         let itemToAppend = 
             '<div class="col s4">'+
-            '<div class="card small"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+            '<div class="card small"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
             '</div><div class="card-content">'+
-            '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+ '<div class="card-reveal">'+
+            '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.color+'</a></p></div>'+ '<div class="card-reveal">'+
             '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ '<p class="card-text grey-text">'+item.desciption+'</p>'+
             '</div></div></div>';
         $("#card-section").append(itemToAppend);
     })
 };
 
+const postcards = (formData) => {
+    // make a request to store the cat data
+    $.post('/api/cats', formData, (response) => {
+        if (response.statusCode == 201) {
+            console.log('Cat added successfully:', response.data);
+            alert('Cat added successfully');
+        } else {
+            console.error('Error adding cat:', response.message);
+            alert('Error adding cat');
+        }
+    });
+}
+
+const getcards = () => {
+    // make a request to the server to get the cats data
+    $.get('/api/cats',(response) => {
+        if (response.statusCode==200){
+            // display the cats as the cards
+            addCards(response.data);
+        }
+    })
+}
+
 const submitForm = () => {
     let formData = {};
-    formData.firstName = $('#first_name').val();
-    formData.lastName = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-    console.log("form submitted ", formData);
+    formData.title = $('#title').val();
+    formData.color = $('#color').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
+
+    postcards(formData);
 }
 
 $(document).ready(function() {
     $('.materialboxed-image').materialbox();
-    addCards(cardList);
     $('#formSubmit').click(()=>{ 
         submitForm();
     })
+    getcards();
     $('.modal').modal();
 });
