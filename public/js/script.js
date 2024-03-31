56214608b68ceaa8f582f9cec8d8e99e72d06ebe
Joe Cin Ng <joe5763481@gmail.com>
2024-04-01 09:33:53 +1100

    minor changes

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ const addCards = (items) => {
             '<div class="card small"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
             '</div><div class="card-content">'+
             '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.color+'</a></p></div>'+ '<div class="card-reveal">'+
-            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ '<p class="card-text grey-text">'+item.desciption+'</p>'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ '<p class="card-text grey-text">'+item.description+'</p>'+
             '</div></div></div>';
         $("#card-section").append(itemToAppend);
     })
