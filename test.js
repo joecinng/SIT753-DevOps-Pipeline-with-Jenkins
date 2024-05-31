bab06975191b21dec953fc562157630eebe9ccf2
Joe Cin Ng <joe5763481@gmail.com>
2024-05-31 14:55:34 +1000

    Update test.js

--- test.js
+++ test.js
@@ -11,7 +11,7 @@ const url = `${baseUrl}?token=${token}`;
 describe("Get api/cats", function() 
 {
     it("returns status 200 to check if api works", function(done) {
-        request({ url: url, headers: authHeader }, function(error, response, body) {
+        request({ url: url }, function(error, response, body) {
             console.log(url);
             console.log("error: "+ error);
             console.log("response: "+response);
@@ -24,7 +24,7 @@ describe("Get api/cats", function()
     });
 
     it("Returns the message as the result", function(done) {
-        request({ url: url, headers: authHeader }, function(error, response, body) {
+        request({ url: url }, function(error, response, body) {
             // parse the json body string to an object to retrieve message and status code
             body = JSON.parse(body);
             expect(body.message).to.be.equal("get all cards success");
@@ -38,14 +38,14 @@ describe("Post api/cats", function()
     var newCat = new CatModel("Kitten test", "brown", "http://cat.jpg", "A test kitten");
 
     it("returns status 201 to check if api works", function(done) {
-        request({ url: url, headers: authHeader, json: newCat }, function(error, response, body) {
+        request({ url: url, json: newCat }, function(error, response, body) {
             expect(response.statusCode).to.be.equal(201);
             done()
         });
     });
 
     it("Adds a new cat", function(done) {
-        request({ url: url, headers: authHeader, json: newCat }, function(error, response, body) {
+        request({ url: url, json: newCat }, function(error, response, body) {
             expect(body.message).to.be.equal("Cat created successfully");
             done()
         });
