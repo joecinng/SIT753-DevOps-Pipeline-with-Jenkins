60fa3e5e0c3639a1ee5e37139ae69b58e8bdb494
Joe Cin Ng <joe5763481@gmail.com>
2024-05-31 13:51:58 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var request = require("request");
 var expect = require('chai').expect;
 const { CatModel } = require('./Models/catModel');
-const url = "http://localhost:3000/api/cats";
+const url = "http://localhost:8083/api/cats";
 
 describe("Get api/cats", function() 
 {
