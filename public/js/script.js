2617e780123ee688839b898664a66cfe9c547c7c
Joe Cin Ng <joe5763481@gmail.com>
2024-05-31 10:36:34 +1000

    minor fix on submission cat

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,42 @@
 const addCards = (items) => { 
+    $("#card-section").empty();
     items.forEach(item => {
         let itemToAppend = 
             '<div class="col s4">'+
             '<div class="card small"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.path+'">'+
             '</div><div class="card-content">'+
-            '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.color+'</a></p></div>'+ '<div class="card-reveal">'+
-            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ '<p class="card-text grey-text">'+item.description+'</p>'+
+            '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.color+'</a></p></div>'+ 
+            '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ 
+            '<p class="card-text grey-text">'+item.description+'</p>'+
             '</div></div></div>';
         $("#card-section").append(itemToAppend);
-    })
+    });
 };
 
 const postcards = (formData) => {
     // make a request to store the cat data
-    $.post('/api/cats', formData, async (response) => {
+    $.post('/api/cats', formData, (response) => {
         if (response.statusCode == 201) {
             console.log('Cat added successfully:', response.data);
-            await alert('Cat added successfully');
+            alert('Cat added successfully');
+            getcards();
+            $('#modal1').modal('close'); // Changed to 'close'
         } else {
             console.error('Error adding cat:', response.message);
-            await alert('Error adding cat');
+            alert('Error adding cat');
         }
     });
 }
 
 const getcards = () => {
     // make a request to the server to get the cats data
-    $.get('/api/cats',(response) => {
-        if (response.statusCode==200){
+    $.get('/api/cats', (response) => {
+        if (response.statusCode == 200) {
             // display the cats as the cards
             addCards(response.data);
         }
-    })
+    });
 }
 
 const submitForm = () => {
@@ -51,9 +56,9 @@ $(document).ready(function() {
         console.log('Random Number: ', + msg);
     });
     $('.materialboxed-image').materialbox();
-    $('#formSubmit').click(()=>{ 
+    $('#formSubmit').click(() => { 
         submitForm();
-    })
+    });
     getcards();
     $('.modal').modal();    
 });
