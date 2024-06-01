b22a465498b495dbb9d4139cad665002f3bc378a
Joe Cin Ng <joe5763481@gmail.com>
2024-06-01 17:26:44 +1000

    updates

---
+++ sonar-scanner.sh
@@ -0,0 +1,5 @@
+C:/"Program Files"/sonar-scanner-cli-5.0.1.3006-windows/sonar-scanner-5.0.1.3006-windows/bin/sonar-scanner.bat 
+-D"sonar.projectKey=local-project" 
+-D"sonar.sources=." 
+-D"sonar.host.url=http://localhost:9000" 
+-D"sonar.token=sqp_1e81ca67b39b85e12c6f4d7e38a12822165574aa"
